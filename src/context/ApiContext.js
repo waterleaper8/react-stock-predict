f7428130920f99bccee95a636785864899b8ad62
waterleaper <waterleaper8@gmail.com>
2022-10-15 15:24:10 +0900

    例外処理を追加

--- src/context/ApiContext.js
+++ src/context/ApiContext.js
@@ -20,10 +20,14 @@ const ApiContextProvider = (props) => {
   const [price, setPrice] = useState(data)
   const [predict, setPredict] = useState(0)
   const [yesterday, setYesterday] = useState(0)
+  const [errorData, setErrorData] = useState("")
+  const [errorPredict, setErrorPredict] = useState("")
+  const [predictIsLoading, setPredictIsLoading] = useState(false)
 
   const fetchStockPrice = useCallback(
     (code) => {
-      axios // .get(`http://192.168.11.87:8000/api/stockprice/?code=${code}`)
+      setPredictIsLoading(true)
+      axios
         // .get(`http://127.0.0.1:8000/api/stock_data/?code=${code}`)
         .get(`https://stock-api.waterleaper.net/fetch_stock_data?code=${code}`)
         .then((res) => {
@@ -52,6 +56,7 @@ const ApiContextProvider = (props) => {
         })
         .catch((error) => {
           console.log(error)
+          setErrorData("株価データの取得に失敗しました")
         })
     },
     [selectedCode]
@@ -59,15 +64,16 @@ const ApiContextProvider = (props) => {
 
   const fetchPredict = useCallback((code) => {
     axios
-      // .get(`http://192.168.11.87:8000/api/stockprice/?code=${code}`)
       // .get(`http://127.0.0.1:8000/api/predict/?code=${code}`)
       .get(`https://stock-api.waterleaper.net/fetch_predict_value?code=${code}`)
       .then((res) => {
         console.log(res.status)
         setPredict(res.data)
+        setPredictIsLoading(false)
       })
       .catch((error) => {
         console.log(error)
+        setErrorPredict("予測値の取得に失敗しました")
       })
   }, [])
 
@@ -98,6 +104,10 @@ const ApiContextProvider = (props) => {
         setPredict,
         yesterday,
         setYesterday,
+        errorData,
+        errorPredict,
+        predictIsLoading,
+        setPredictIsLoading,
       }}
     >
       {props.children}

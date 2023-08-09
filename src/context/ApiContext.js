b1149ca581b116bf0086167f2e0503bf4e846eac
waterleaper <waterleaper8@gmail.com>
2023-08-09 15:41:47 +0900

    change-api-path

--- src/context/ApiContext.js
+++ src/context/ApiContext.js
@@ -20,14 +20,10 @@ const ApiContextProvider = (props) => {
   const [price, setPrice] = useState(data)
   const [predict, setPredict] = useState(0)
   const [yesterday, setYesterday] = useState(0)
-  const [errorData, setErrorData] = useState("")
-  const [errorPredict, setErrorPredict] = useState("")
-  const [predictIsLoading, setPredictIsLoading] = useState(false)
 
   const fetchStockPrice = useCallback(
     (code) => {
-      setPredictIsLoading(true)
-      axios
+      axios // .get(`http://192.168.11.87:8000/api/stockprice/?code=${code}`)
         // .get(`http://127.0.0.1:8000/api/stock_data/?code=${code}`)
         // .get(`https://stock-api.waterleaper.net/fetch_stock_data?code=${code}`)
         .get(`https://waterleaper.net/api/stock/fetch_stock_data?code=${code}`)
@@ -57,7 +53,6 @@ const ApiContextProvider = (props) => {
         })
         .catch((error) => {
           console.log(error)
-          setErrorData("株価データの取得に失敗しました")
         })
     },
     [selectedCode]
@@ -65,17 +60,16 @@ const ApiContextProvider = (props) => {
 
   const fetchPredict = useCallback((code) => {
     axios
+      // .get(`http://192.168.11.87:8000/api/stockprice/?code=${code}`)
       // .get(`http://127.0.0.1:8000/api/predict/?code=${code}`)
       // .get(`https://stock-api.waterleaper.net/fetch_predict_value?code=${code}`)
       .get(`https://waterleaper.net/api/stock/fetch_predict_value?code=${code}`)
       .then((res) => {
         console.log(res.status)
         setPredict(res.data)
-        setPredictIsLoading(false)
       })
       .catch((error) => {
         console.log(error)
-        setErrorPredict("予測値の取得に失敗しました")
       })
   }, [])
 
@@ -106,10 +100,6 @@ const ApiContextProvider = (props) => {
         setPredict,
         yesterday,
         setYesterday,
-        errorData,
-        errorPredict,
-        predictIsLoading,
-        setPredictIsLoading,
       }}
     >
       {props.children}
